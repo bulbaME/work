8eb49ae61459dcdcb3bef6e3fa8908f88841632d
bulbaME <65255962+bulbaME@users.noreply.github.com>
2023-07-25 18:31:51 +0300

    Update send_mail.js

--- seo-digital-360/app/_modules/send_mail.js
+++ seo-digital-360/app/_modules/send_mail.js
@@ -7,6 +7,6 @@ export default async function send_mail(name, mail, product, text) {
         from: 'no-reply@seodigital360.com',
         to: 'support@seodigital360.com',
         subject: 'A person contacted',
-        html: `<p>name: {name}</p> <p>e-mail: {mail}</p> <p>product: {product == 'Select service' ? 'none':product}</p> <p>comments: {text}</p>`
+        html: `<p>name: ${name}</p> <p>e-mail: ${mail}</p> <p>product: ${product == 'Select service' ? 'none':product}</p> <p>comments: ${text}</p>`
     }, () => {});
 }
