02e26806324ab36a9c87e040cba16b7757c61ad0
bulbaME <65255962+bulbaME@users.noreply.github.com>
2023-07-31 22:41:49 +0300

    Update send_form.js

--- translation-international/pages/api/send_form.js
+++ translation-international/pages/api/send_form.js
@@ -4,8 +4,8 @@ const send_mail = (name, mail, product, text) => {
     sendmail({
         from: 'no-reply@translation-international.ro',
         to: 'support@translation-international.ro',
-        subject: `A person contacted \nName: ${name}\nE-mail: ${mail}\nService they need: ${product}`,
-        html: text
+        subject: 'A person contacted',
+        html: `<p>name: ${name}</p> <p>e-mail: ${mail}</p> <p>product: ${product == 'Select service' ? 'none' : product}</p> <p>comments: ${text}</p>`
     }, () => {});
 }
 
