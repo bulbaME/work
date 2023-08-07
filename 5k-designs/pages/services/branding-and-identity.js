3fdabee32f0bcef48ab287a8cca4c0c7e3c691c1
bulbaME <65255962+bulbaME@users.noreply.github.com>
2023-08-07 10:30:18 +0300

    update

--- 5k-designs/pages/services/branding-and-identity.js
+++ 5k-designs/pages/services/branding-and-identity.js
@@ -1,6 +1,6 @@
 import Footer from "@/modules/footer";
 import Header from "@/modules/header";
-import { WCta, WService } from "@/modules/widgets";
+import { P1L, WCta, WService } from "@/modules/widgets";
 
 import { services } from "@/public/json/data.json";
 
@@ -18,11 +18,7 @@ export default function X() {
             </div>
         </div>
         <WService s={s.services} />
-        <div className='max-w-7xl m-auto sm:p-14 p-6 sm:text-lg font-semibold'>
-            <div className='border-[1px] p-4 border-black'>
-                {s.text_long}
-            </div>
-        </div>
+        <P1L img='11' slogan='Creating Timeless Brand Identities' title='Brand Brilliance' text={s.text_long} />
         <WCta title='Get Started with 5K Designs' cta="Let's Get Started" route='/contact#contact-us' text="Ready to embark on a design journey that celebrates creativity and drives results? Get started today, and together, we'll craft something extraordinary for your brand, bringing your vision to life. Our collaborative approach ensures that your ideas blend seamlessly with our expertise, resulting in designs that exceed your expectations. Let's create a lasting impact together and set your brand on a path of success that will leave a legacy for years to come." />
         <Footer />
     </>;
